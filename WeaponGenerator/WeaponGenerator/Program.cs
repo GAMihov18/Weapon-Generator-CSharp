9372ac0ac5f1f3c631d64d2cb86255a3daeaf467
RoyalFang <GAMihov18@codingburgas.bg>
2021-06-10 00:51:02 +0300

    Add weapon generation

---
+++ WeaponGenerator/WeaponGenerator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using WeaponGeneration;
+
+namespace WeaponGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Weapon weapon;
+            StreamWriter writer = new StreamWriter("generated_weapons.txt");
+            string a = "Name:Assembly Damage:Physical Damage:Magical Damage:Crit Mult:Crit Rate:Rarity:Weapon Type:Main Damage Type:Physical Damage Type:Magical Damage Type\n";
+            using (writer)
+            {
+                writer.Write(a);
+                Stopwatch timer = Stopwatch.StartNew();
+                for (int i = 0; i < 1000; i++)
+                {
+                    weapon = new Weapon();
+                    writer.Write(weapon.GetWeaponData(false));
+                }
+                timer.Stop();
+                TimeSpan tm = timer.Elapsed;
+                Console.WriteLine("Time elapsed:{0:#0} Minutes, {1:#0} Seconds, {2:##0} Milliseconds", tm.Minutes, tm.Seconds, tm.Milliseconds);
+            }
+            Console.WriteLine("Press any key to exit application..."); Console.ReadKey();
+            
+        }
+    }
+}
---
+++ WeaponGenerator/WeaponGenerator/RandomNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumber
+{
+    public struct RAND
+    {
+        static Random rnd = new Random();
+        public static int getRandInt(int a, int b)
+        {
+            return rnd.Next(a, b);
+        }
+        public static double getRandDouble(double a, double b)
+        {
+            return rnd.NextDouble() * (b - a) + a;
+        }
+    }
+}
