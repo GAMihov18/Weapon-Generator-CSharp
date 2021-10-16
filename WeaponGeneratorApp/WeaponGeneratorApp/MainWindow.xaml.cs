3f6a2d07230c8b8b258940f0c73c0d11fb8ebb0d
Georgi Mihov <GAMihov18@codingburgas.bg>
2021-10-16 09:54:36 -0700

    Add text document with ideas

--- WeaponGeneratorApp/WeaponGeneratorApp/MainWindow.xaml.cs
+++ WeaponGeneratorApp/WeaponGeneratorApp/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using RFMiscLib.RandomNumber;
-using WeaponGeneration;
+using Generation.Weapons;
 using LogSystem;
 namespace WeaponGenerator
 {
--- WeaponGeneratorApp/WeaponGeneratorApp/WeaponInfo.xaml.cs
+++ WeaponGeneratorApp/WeaponGeneratorApp/WeaponInfo.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using WeaponGeneration;
+using Generation.Weapons;
 namespace WeaponGenerator
 {
     /// <summary>
