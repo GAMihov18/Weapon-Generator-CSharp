25d7ea819501a26c13fc12764ac37e1e23162d4d
Georgi Mihov <GAMihov18@codingburgas.bg>
2021-11-27 15:41:19 +0200

    Start implementation of Log Window

---
+++ WeaponGeneratorApp/WeaponGeneratorApp/LogWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using LogSystem;
+namespace WeaponGeneratorApp
+{
+    /// <summary>
+    /// Interaction logic for LogWindow.xaml
+    /// </summary>
+    public partial class LogWindow : Window
+    {
+        Log logFile = new Log("logFile.txt", true);
+        public LogWindow()
+        {
+            logFile.Clear();
+            InitializeComponent();
+        }
+        public void Clear()
+        {
+            logFile.Clear();
+            logView.Items.Clear();
+        }
+        public void Clear_Click(object sender, EventArgs ev)
+        {
+            Clear();
+        }
+        public void LogText(string msg)
+        {
+        }
+        public void LogInfo(string msg)
+        {
+
+        }
+        public void LogWarning(string msg)
+        {
+
+        }
+        public void LogError(string msg)
+        {
+
+        }
+        public void LogFatal(string msg)
+        {
+
+        }
+    }
+}
--- WeaponGeneratorApp/WeaponGeneratorApp/MainWindow.xaml.cs
+++ WeaponGeneratorApp/WeaponGeneratorApp/MainWindow.xaml.cs
@@ -24,45 +24,50 @@ namespace WeaponGenerator
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		Log logFile = new Log("log.txt",true);
-		List<Weapon> weapons = new List<Weapon>();
+		Log logFile = new Log("log.txt", true);
+		static List<Weapon> weapons = new List<Weapon>();
 		List<ListViewItem> listViewItems = new List<ListViewItem>();
-		int totalGeneratedWeapons = 0;
-		Log log = new Log("log.txt",true);
-		
+
 		public MainWindow()
 		{
-			log.LogInfo($"Log created on: {DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")}");
+			logFile.LogInfo($"Log created on: {DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")}");
 			InitializeComponent();
 		}
-
-		private void weaponItem_DoubleClick(object sender, MouseButtonEventArgs e)
+		delegate void Caller(object sender,MouseButtonEventArgs ev);
+		private void weaponItem_DoubleClick(object sender, EventArgs ev)
 		{
+			MouseButtonEventArgs e = ev as MouseButtonEventArgs;
 			ListViewItem item = sender as ListViewItem;
-			WeaponInfoTB.Text = weapons[(int)item.Tag].GetWeaponData(true);
-
+			new WeaponInfo(weapons[(int)item.Tag]).Show();
 		}
-		private void NumberOfWeapons_KeyPressed(object sender, KeyEventArgs e)
+		private void NumberOfWeapons_KeyPressed(object sender, EventArgs ev)
 		{
+			KeyEventArgs e = ev as KeyEventArgs;
 			if (e.Key == Key.Enter)
 			{
 				GenerateWeaponsButton_Click(sender, e);
 			}
 		}
-		private void GenerateWeaponsButton_Click(object sender, RoutedEventArgs e)
+		private void GenerateWeaponsButton_Click(object sender, EventArgs ev)
 		{
+			RoutedEventArgs e = ev as RoutedEventArgs;
 			int num;
 			Log weaponLog = new Log("generatedWeapons.txt", true);
 			weaponLog.Clear();
 			if (int.TryParse(NumberOfWeaponsTB.Text, out num))
 			{
+				logFile.LogInfo($"Generating {num} weapons");
+				//Clear previous generations
 				listOfWeapons.Items.Clear();
-				DateTime start = DateTime.Now;
 				weapons.Clear();
+				//Start generation timer
+				DateTime start = DateTime.Now;
+				
 				for (int i = 0; i < num; i++)
 				{
 					weapons.Add(new Weapon());
 				}
+
 				if ((bool)SaveWeapon.IsChecked)
 				{
 					weaponLog.LogText("Name:Assembly Damage:Physical Damage:Magical Damage:Crit Multiplier:Crit Chance:Rarity:Weapon Type:Main Damage Type: Physical Damage Type:Magical Damage Type");
@@ -86,8 +91,9 @@ namespace WeaponGenerator
 				MessageBoxImage icon = MessageBoxImage.Information;
 				MessageBoxButton button = MessageBoxButton.OK;
 				MessageBox.Show("Weapons successfully generated", "Info", button, icon);
+				//End generation timer and log in logfile
 				DateTime end = DateTime.Now;
-				logFile.LogText((end - start).ToString());
+				logFile.LogInfo($"Time taken to generate {num} weapons: {end - start}");
 			}
 			else
 			{
@@ -97,41 +103,43 @@ namespace WeaponGenerator
 			}
 			NumberOfWeaponsTB.Clear();
 		}
-		private void NumberOfArmorsTB_KeyDown(object sender, KeyEventArgs e)
+		private void NumberOfArmorsTB_KeyDown(object sender, EventArgs ev)
 		{
+			KeyEventArgs e = ev as KeyEventArgs;
 			if (e.Key == Key.Enter)
 			{
 				GenerateArmorsButton_Click(sender, e);
 			}
 		}
 
-		private void GenerateArmorsButton_Click(object sender, RoutedEventArgs e)
+		private void GenerateArmorsButton_Click(object sender, EventArgs ev)
 		{
 
 		}
 
-		private void NumberOfPlayersTB_KeyDown(object sender, KeyEventArgs e)
+		private void NumberOfPlayersTB_KeyDown(object sender, EventArgs ev)
 		{
+			KeyEventArgs e = ev as KeyEventArgs;
 			if (e.Key == Key.Enter)
 			{
 				GeneratePlayersButton_Click(sender, e);
 			}
 		}
 
-		private void GeneratePlayersButton_Click(object sender, RoutedEventArgs e)
+		private void GeneratePlayersButton_Click(object sender, EventArgs ev)
 		{
 
 		}
 
-		private void GeneratorMenuButton_Click(object sender, RoutedEventArgs e)
+		private void GeneratorMenuButton_Click(object sender, EventArgs ev)
 		{
 			MainMenu.Visibility = Visibility.Collapsed;
 			GeneratorMenu.Visibility = Visibility.Visible;
 		}
 
-		private void ChoosePlayerGeneration_Click(object sender, RoutedEventArgs e)
+		private void ChoosePlayerGeneration_Click(object sender, EventArgs ev)
 		{
-
+			logFile.LogInfo("Player Menu Chosen");
 			WeaponGeneratorGrid.Visibility = Visibility.Collapsed;
 			ArmorGeneratorGrid.Visibility = Visibility.Collapsed;
 			PlayerGeneratorGrid.Visibility = Visibility.Visible;
@@ -140,7 +148,7 @@ namespace WeaponGenerator
 			ChooseArmorGeneration.Visibility = Visibility.Visible;
 		}
 
-		private void ChooseArmorGeneration_Click(object sender, RoutedEventArgs e)
+		private void ChooseArmorGeneration_Click(object sender, EventArgs ev)
 		{
 			PlayerGeneratorGrid.Visibility = Visibility.Collapsed;
 			WeaponGeneratorGrid.Visibility = Visibility.Collapsed;
@@ -150,7 +158,7 @@ namespace WeaponGenerator
 			ChooseWeaponGeneration.Visibility = Visibility.Visible;
 		}
 
-		private void ChooseWeaponGeneration_Click(object sender, RoutedEventArgs e)
+		private void ChooseWeaponGeneration_Click(object sender, EventArgs ev)
 		{
 			PlayerGeneratorGrid.Visibility = Visibility.Collapsed;
 			ArmorGeneratorGrid.Visibility = Visibility.Collapsed;
@@ -160,15 +168,15 @@ namespace WeaponGenerator
 			ChooseWeaponGeneration.Visibility = Visibility.Collapsed;
 		}
 
-		private void ChooseBackToMainMenu_Click(object sender, RoutedEventArgs e)
+		private void ChooseBackToMainMenu_Click(object sender, EventArgs ev)
 		{
 			GeneratorMenu.Visibility = Visibility.Collapsed;
 			MainMenu.Visibility = Visibility.Visible;
 		}
 
-		private void ExitButton_Click(object sender, RoutedEventArgs e)
+		private void ExitButton_Click(object sender, EventArgs ev)
 		{
-			log.LogInfo("End of log\n-----------------");
+			logFile.LogInfo("End of log\n-----------------");
 			Application.Current.Shutdown();
 		}
 	}
