b1f4d2f04eb4cea2d3ff4374c335bfa4f9d9208a
Georgi Mihov <georgi.mihov04@gmail.com>
2026-05-06 16:10:19 +0200

    Edit dependencies and add more to eleementTypes

--- RPG Project/GameCore/ElementType.cs
+++ RPG Project/GameCore/ElementType.cs
@@ -1,9 +1,11 @@
 namespace GameCore;
 //TODO Add more properties for armor modifiers once I come up with the values that they will have.
-public class ElementType(bool weaponCompatible, bool armorCompatible, double physicalDamageModFlat, double physicalDamageModPercent, double magicalDamageModFlat, double magicalDamageModPercent)
+public class ElementType(bool weaponCompatible, bool armorCompatible, bool physicalType, bool magicalType, double physicalDamageModFlat, double physicalDamageModPercent, double magicalDamageModFlat, double magicalDamageModPercent)
 {
 	public bool WeaponCompatible => weaponCompatible;
 	public bool ArmorCompatible => armorCompatible;
+	public bool PhysicalType => physicalType;
+	public bool MagicalType => magicalType;
 	public double WeaponPhysicalDamageModFlat => physicalDamageModFlat;
 	public double WeaponPhysicalDamageModPercent => physicalDamageModPercent;
 	public double WeaponMagicalDamageModFlat => magicalDamageModFlat;
